b1c3793d306c345826af04361ff34712c2add93f
ahmednbail <116032681+ahmednbail@users.noreply.github.com>
2025-08-23 03:23:06 +0300

    I have finished all transfer Dags

---
+++ dags/Master_Dag.py
@@ -0,0 +1,62 @@
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+from airflow.utils.task_group import TaskGroup
+from airflow import DAG
+
+
+default_args = {
+"owner": "Ahmed Nabil",
+"depends_on_past": False,
+"start_date": datetime(2025, 5, 23),
+"bigquery_conn_id": "bigquery_default",
+"max_active_runs": 1,
+"retries": 1,
+}
+
+
+dag = DAG(
+    dag_id="master_transfer_dag",
+    description="All transfer DAGs",
+    schedule_interval="05 00 * * *",
+    concurrency=6,
+    max_active_runs=1,
+    default_args=default_args,
+    tags=["master_transfer"],
+    catchup=False,
+ )
+
+ext_date = "{{ execution_date }}"
+
+
+def create_trigger_task(trigger_dag_id):
+     return TriggerDagRunOperator(
+         task_id=trigger_dag_id,
+         dag=dag,
+         trigger_dag_id=trigger_dag_id,
+         execution_date=ext_date,
+         wait_for_completion=True,
+         poke_interval=30,
+         deferrable=True,
+         trigger_rule="all_done",
+     )
+
+with TaskGroup(group_id="P1_pipelines", dag=dag) as P1_pipelines:
+     p1_dags = [
+        "customers_db_transfer",
+        "orders_db_transfer",
+        "order_items_db_transfer",
+        "order_payments_db_transfer",
+     ]
+     for task_id in p1_dags:
+         create_trigger_task(task_id)
+
+with TaskGroup(group_id="P2_pipelines", dag=dag) as P2_pipelines:
+     p2_dags = [
+        "products_db_transfer",
+        "sellers_db_transfer",
+        "geolocation_db_transfer",
+     ]
+     for task_id in p2_dags:
+         create_trigger_task(task_id)
+
+
