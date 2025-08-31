f0196a63c1d1ee2bad6802d55669e34bf41def62
ahmednbail <116032681+ahmednbail@users.noreply.github.com>
2025-08-31 09:30:39 +0300

    Changes

--- dags/Transfer_DAG/Ahmed_Master_Dag.py
+++ dags/Transfer_DAG/Ahmed_Master_Dag.py
@@ -3,16 +3,14 @@ from datetime import datetime
 from airflow.utils.task_group import TaskGroup
 from airflow import DAG
 
-
 default_args = {
-"owner": "Ahmed Nabil",
-"depends_on_past": False,
-"start_date": datetime(2025, 5, 23),
-"bigquery_conn_id": "bigquery_default",
-"max_active_runs": 1,
-"retries": 1,
+    "owner": "Ahmed Nabil",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 5, 23),
+    "bigquery_conn_id": "bigquery_default",
+    "max_active_runs": 1,
+    "retries": 1,
 }
-
 
 dag = DAG(
     dag_id="Ahmed_master_transfer_dag",
@@ -22,8 +20,8 @@ dag = DAG(
     max_active_runs=1,
     default_args=default_args,
     tags=["master_transfer"],
-    catchup=True,
- )
+    catchup=False,  
+)
 
 ext_date = "{{ execution_date }}"
 
@@ -31,7 +29,7 @@ def create_trigger_task(trigger_dag_id):
     return TriggerDagRunOperator(
         task_id=trigger_dag_id,
         trigger_dag_id=trigger_dag_id,
-        conf={"execution_date": ext_date},   
+        conf={"execution_date": ext_date},
         wait_for_completion=True,
         poke_interval=30,
         deferrable=True,
@@ -39,24 +37,27 @@ def create_trigger_task(trigger_dag_id):
     )
 
 with TaskGroup(group_id="P1_pipelines", dag=dag) as P1_pipelines:
-     p1_dags = [
+    p1_dags = [
         "customers_db_transfer",
         "orders_db_transfer",
         "order_items_db_transfer",
         "ahmednabil_order_payments_api_dag",
-
-     ]
-     for task_id in p1_dags:
-         create_trigger_task(task_id)
+    ]
+    for task_id in p1_dags:
+        create_trigger_task(task_id)
 
 with TaskGroup(group_id="P2_pipelines", dag=dag) as P2_pipelines:
-     p2_dags = [
+    p2_dags = [
         "products_db_transfer",
         "ahmednabil_seller_api_dag",
         "geolocation_db_transfer",
-     ]
-     for task_id in p2_dags:
-         create_trigger_task(task_id)
+    ]
+    for task_id in p2_dags:
+        create_trigger_task(task_id)
+
+
+P1_pipelines >> P2_pipelines
+
          
 
 
